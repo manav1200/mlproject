187cd7c82fd36ae5ae7426bcd3b07d72adb9be40
Manav Gupta <manavgupta1200@gmail.com>
2026-02-07 03:44:31 +0530

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,9 @@ from src.exception import CustomException
 import dill
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
+os.environ["LOKY_MAX_CPU_COUNT"] = "6"
 
 def save_object(file_path, obj):
     try:
@@ -21,16 +24,23 @@ def save_object(file_path, obj):
         raise CustomException(e,sys)
     
 
-def evaluate_models(x_train,y_train,x_test,y_test,models):
+def evaluate_models(x_train,y_train,x_test,y_test,models,param):
     try:
         report= {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
 
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(x_train,y_train)
+
+            #model.fit(x_train,y_train)
             y_train_pred = model.predict(x_train)
-            y_test_pred =model.predict(x_test)
+            y_test_pred = model.predict(x_test)
 
             train_model_score = r2_score(y_train,y_train_pred)
 
